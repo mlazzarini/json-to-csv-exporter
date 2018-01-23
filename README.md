443af6ff6a68b4912845ee4627cab845afb3dea1
mlazzarini <margherita.lazzarini@gmail.com>
2018-01-23 15:49:54 +0100

    Make tool actually runnable

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # json-to-csv-exporter
+
+Giving a file containing line-separated JSON events (Kafka export), provides a CSV containing tenant, user_id and timestamp information related to `reports/downloaded` events.
+
+## Start
+First run:
+```bash
+$ npm install
+```
+then:
+```bash
+$ node index.js --file-name INPUT_FILE_NAME
+```
--- index.js
+++ index.js
@@ -5,24 +5,25 @@
 */
 
 const fs = require('fs');
+const path = require('path');
 const json2csv = require('json2csv');
 const logger = require('log4js').getLogger();
 const IncrementalJSONParser = require('incremental-json-parser');
 
 const argv = require('yargs')
-	.option('kafka', {
-		default: 'localhost:9092',
-		describe: 'Kafka address',
+	.option('file-name', {
+		default: 'test-events.txt',
+		describe: 'Input file name',
 		type: 'string',
 	})
 	.help()
 	.argv;
 
-const FILE = './test-events.txt';
+const file = path.resolve(process.cwd(), argv.fileName);
 
 logger.info('Export-tool started');
 
-const messageJson = fs.readFileSync(FILE, 'UTF-8');
+const messageJson = fs.readFileSync(file, 'UTF-8');
 
 const events = [];
 
@@ -42,7 +43,7 @@ const lines = messageJson.split('\n')
 const fields = ['_meta.tenant', '_meta.user_id', '_meta.timestamp'];
 const csv = json2csv({data: events, fields});
 
-fs.writeFile('output.csv', csv, err => {
+fs.writeFile('output2.csv', csv, err => {
 	if (err) {
 		throw err;
 	}
